9b05573d896e130a8439f44f1048dd7439c5b37f
Android Studio <devandroid@ort>
2025-07-16 09:17:38 -0300

    nahiapinga

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import authRoutes from './routes/auth.routes.js';
 import eventRoutes from './routes/event.routes.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors()); 
 
 app.use(express.json());
 
